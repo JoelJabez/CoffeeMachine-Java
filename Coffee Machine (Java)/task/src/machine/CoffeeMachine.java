03119f56d70499b424b5780f70809241745d946c
joel0134 <joeldavethu@gmail.com>
2023-06-23 13:31:37 +0800

    Jetbrains Academy, Coffee Machine(Part 3/6)

--- Coffee Machine (Java)/task/src/machine/CoffeeMachine.java
+++ Coffee Machine (Java)/task/src/machine/CoffeeMachine.java
@@ -6,17 +6,37 @@ public class CoffeeMachine {
     public static void main(String[] args) {
 	    Scanner scanner = new Scanner(System.in);
 
+		// Declaring and initializing constant per cup
+		final int amountOfWaterPerCup = 200;
+		final int amountOfMilkPerCup = 50;
+		final int amountOfCoffeeBeansPerCup = 15;
+
+		// Getting input from the user
+	    System.out.println("Write how many ml of water the coffee beans has: ");
+	    final int amountOfWater = scanner.nextInt();
+
+	    System.out.println("Write how many ml of milk the coffee beans has: ");
+	    final int amountOfMilk = scanner.nextInt();
+
+	    System.out.println("Write how many grams of coffee beans the coffee beans has: ");
+	    final int amountOfCoffeeBeans = scanner.nextInt();
+
 	    System.out.println("Write how many cups of coffee you will need: ");
-		final int cups = scanner.nextInt();
+		final int amountOfCups = scanner.nextInt();
 
-		final int amountOfWater = cups * 200;
-		final int amountOfMilk = cups * 50;
-		final int amountOfCoffeeBeans = cups * 15;
+		// Dividing the amount from the user with the constant
+		int minimumAmountOfWater = amountOfWater / amountOfWaterPerCup;
+	    int minimumAmountOfMilk = amountOfMilk / amountOfMilkPerCup;
+	    int minimumAmountOfCoffeeBeans = amountOfCoffeeBeans / amountOfCoffeeBeansPerCup;
 
-	    System.out.printf("For %d cups of coffee you will need: \n", cups);
+		// Getting the minimum number
+		int minimumCups = Math.min(minimumAmountOfWater, Math.min(minimumAmountOfMilk, minimumAmountOfCoffeeBeans));
 
-	    System.out.printf("%d ml of water\n", amountOfWater);
-	    System.out.printf("%d ml of milk\n", amountOfMilk);
-	    System.out.printf("%d g of coffee beans\n", amountOfCoffeeBeans);
+		// Printing the message
+		switch (Integer.compare(minimumCups, amountOfCups)) {
+			case 1 -> System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)\n", minimumCups - amountOfCups);
+			case 0 -> System.out.println("Yes, I can make that amount of coffee");
+			case -1 -> System.out.printf("No, I can make only %d cup(s) of coffee", minimumCups);
+		}
     }
 }
